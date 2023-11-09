ed19d5116fd2b595263463e10f4f9f44c0b028eb
Michal Pluta <vsdc48@durham.ac.uk>
2023-11-09 16:21:34 +0000

    Change structure of code into a functional approach

--- vsdc48.py
+++ vsdc48.py
@@ -3,66 +3,75 @@ import numpy as np
 from collections import Counter
 import re
 
+OUT_FILEPATH = 'output_data_test.xlsx'
+
+
 ##### Problem 1
 
-df = pd.read_excel('amazon_laptop_2023.xlsx')
+def initialCleaning(df):
+    # Drop rows where all values are NaN
+    df.dropna(how='all', inplace=True)
 
-# Drop rows where all values are NaN
-df.dropna(how='all', inplace=True)
+    # Convert all rows to lowercase
+    # Reasoning: For consistency in data between columns, Different cases increase the dimensionality of the data.
+    #            One case also allows easy comparisons between data and checking for duplicates.
+    df = df.map(lambda x: x.lower() if isinstance(x, str) else x)
 
-# Convert all rows to lowercase
-# Reasoning: For consistency in data between columns, Different cases increase the dimensionality of the data.
-#            One case also allows easy comparisons between data and checking for duplicates.
-df = df.map(lambda x: x.lower() if isinstance(x, str) else x)
+    # Remove duplicate rows
+    # Reasoning: Redundant information that unnecessarily skews the data
+    df = df.drop_duplicates(ignore_index=True)
 
-# Remove duplicate rows
-# Reasoning: Redundant information that unnecessarily skews the data
-df = df.drop_duplicates(ignore_index=True)
+    # Remove all laptops that don't include any model numbers
+    # Reasoning: Laptops with no model number will be very difficult for the person to find. 
+    #            Many different manufacturers can create
+    df.dropna(subset=['model'], inplace=True)
 
-# Remove all laptops that don't include any model numbers
-# Reasoning: Laptops with no model number will be very difficult for the person to find. 
-#            Many different manufacturers can create
-df.dropna(subset=['model'], inplace=True)
+    return df
 
-# Convert colours to standard colours
-# Reasoning: Reduces variability in the dataset due to misspellings, variations and interpretations
-#            Reducuing the variability also helps with categorization which is use for data exploration and visualization
-#            A part of this process also ensures contextual standardization which is crucial where colours are named uniquely 
-#            for marketing or specific contexts (e.g. thunder black or platinum titan)
-colorHashMap = {
-    r".*(silver|sliver|aluminum|platinum|light titan|platinum titan).*": "silver",
-    r".*(black|dark side of the moon|thunder balck|carbon fiber).*": "black",
-    r".*(white).*": "white",
-    r".*(grey|gray|gary|graphite|mercury|dark ash|dark metallic moon).*": "grey",
-    r".*(red).*": "red",
-    r".*(blue|cobalt|sky|dark teal|apollo|midnight).*": "blue",
-    r".*(green|sage|soft mint|dark moss).*": "green",
-    r".*(gold).*": "gold",
-    r".*(almond|beige mousse|lunar light|dune).*": "beige", #TODO Need to justify this
-    r".*(punk pink|electro punk).*": "pink",
-    r".*(information not available|rgb backlit|touchscreen|evo i7-1260p|acronym).*": np.NaN #TODO extract information from here
-}
-df['color'] = df['color'].replace(colorHashMap, regex=True)
+def standardiseColours(df):
+    # Reasoning: Reduces variability in the dataset due to misspellings, variations and interpretations
+    #            Reducuing the variability also helps with categorization which is use for data exploration and visualization
+    #            A part of this process also ensures contextual standardization which is crucial where colours are named uniquely 
+    #            for marketing or specific contexts (e.g. thunder black or platinum titan)
+    colorHashMap = {
+        r".*(silver|sliver|aluminum|platinum|light titan|platinum titan).*": "silver",
+        r".*(black|dark side of the moon|thunder balck|carbon fiber).*": "black",
+        r".*(white).*": "white",
+        r".*(grey|gray|gary|graphite|mercury|dark ash|dark metallic moon).*": "grey",
+        r".*(red).*": "red",
+        r".*(blue|cobalt|sky|dark teal|apollo|midnight).*": "blue",
+        r".*(green|sage|soft mint|dark moss).*": "green",
+        r".*(gold).*": "gold",
+        r".*(almond|beige mousse|lunar light|dune).*": "beige", #TODO Need to justify this
+        r".*(punk pink|electro punk).*": "pink",
+        r".*(information not available|rgb backlit|touchscreen|evo i7-1260p|acronym).*": np.NaN #TODO extract information from here
+    }
+    df['color'] = df['color'].replace(colorHashMap, regex=True)
 
-# Convert brands to standard brands
-# Reasoning: Similar to standardisation of colours
-brandHashMap = {
-    r".*(enovo).*": "lenovo",
-    r".*(carlisle foodservice products|best notebooks|computer upgrade king|quality refurbished computers|microtella|ctl|lpt|rokc|elo|gizpro|jtd).*": np.NaN,
-    r".*(toughbook).*": "panasonic", #TODO move toughbook to the model later
-    r".*(latitude).*": "dell" #TODO ensure all latitude dells have dell as brand and latitude as model
-}
-df['brand'] = df['brand'].replace(brandHashMap, regex=True)
+    return df
 
-# Remove 'inches' from screen sizes and convert screen sizes to float
-# Reasoning: Removing inches allows the values to be converted from a categorical to a continuous scale.
-#            This allows for better data visualisation attempts.
-df['screen_size'] = df['screen_size'].str.replace(' inches', '').astype(float)
+def standardiseBrands(df):
+    # Reasoning: Similar to standardisation of colours
+    brandHashMap = {
+        r".*(enovo).*": "lenovo",
+        r".*(carlisle foodservice products|best notebooks|computer upgrade king|quality refurbished computers|microtella|ctl|lpt|rokc|elo|gizpro|jtd).*": np.NaN,
+        r".*(toughbook).*": "panasonic", #TODO move toughbook to the model later
+        r".*(latitude).*": "dell" #TODO ensure all latitude dells have dell as brand and latitude as model
+    }
+    df['brand'] = df['brand'].replace(brandHashMap, regex=True)
 
+    return df
 
-# Adjust harddisk sizes to be more consistent
-# TODO check the correctness of this function, especially with NaNs
-def convert_to_gb(size, tb_possible:bool = False):
+def cleanScreenSizes(df):
+    # Remove 'inches' from screen sizes and convert screen sizes to float
+    # Reasoning: Removing inches allows the values to be converted from a categorical to a continuous scale.
+    #            This allows for better data visualisation attempts.
+    df['screen_size'] = df['screen_size'].str.replace(' inches', '').astype(float)
+
+    return df
+
+def convert_to_gb(size, tb_possible = False):
+    # TODO check the correctness of this function, especially with NaNs
     size = str(size)
 
     if pd.isna(size): # Check for NaN
@@ -80,13 +89,17 @@ def convert_to_gb(size, tb_possible:bool = False):
         if tb_possible and float(size) < 16:
             return float(size) * 1000
         else: 
-            return float(size)       
-df['harddisk'] = df['harddisk'].apply(convert_to_gb, tb_possible=True)
-df['ram'] = df['ram'].apply(convert_to_gb, tb_possible=False)
+            return float(size)
 
+def convertRamDiskSizesToGB(df):
+    # Adjust harddisk sizes to be more consistent
+    df['harddisk'] = df['harddisk'].apply(convert_to_gb, tb_possible=True)
+    df['ram'] = df['ram'].apply(convert_to_gb, tb_possible=False)
 
-def check_unique_sizes(size: int) -> int:
-    if pd.isna(size): # Check for NaN
+    return df
+
+def bucketStorageSize(size: int) -> int:
+    if pd.isna(size):
         return size
     
     if size in (64, 65):
@@ -107,48 +120,60 @@ def check_unique_sizes(size: int) -> int:
         return 8192
 
     return size
-# df['harddisk'] = df['harddisk'].apply(check_unique_sizes)
+def bucketHardDisk(df):
+    df['harddisk'] = df['harddisk'].apply(bucketStorageSize)
 
+    return df
 
+def standardiseOS(df):
+    # Convert os systems to standard systems
+    osHashMap = {
+        r".*(windows 10 pro|win 10 pro).*": "windows 10 pro",
+        r".*(windows 11 pro).*": "windows 11 pro",
+        r".*(windows 10|win 10).*": "windows 10 home",
+        r".*(windows 11 home|win 11 multi-home).*": "windows 11 home",
+        r".*(windows 7 professional).*": "windows 7 pro"
+    }
+    df['OS'] = df['OS'].replace(osHashMap, regex=True)
 
+    return df
 
-# Convert os systems to standard systems
-osHashMap = {
-    r".*(windows 10 pro|win 10 pro).*": "windows 10 pro",
-    r".*(windows 11 pro).*": "windows 11 pro",
-    r".*(windows 10|win 10).*": "windows 10 home",
-    r".*(windows 11 home|win 11 multi-home).*": "windows 11 home",
-    r".*(windows 7 professional).*": "windows 7 pro"
-}
-df['OS'] = df['OS'].replace(osHashMap, regex=True)
+def normaliseCPUSpeeds(df):
+    # Adjust CPU speeds to be more consistent with the same units
+    def normaliseCPU(cpu_speed):
+        cpu_speed = str(cpu_speed)
+        match = re.search(r'((\d+\.\d+|\d+))', cpu_speed)
+        if match:
+            value = float(match.group())
+            if value > 6:
+                value /= 1000
+  
+    df['cpu_speed'] = df['cpu_speed'].apply(normaliseCPU)
 
+    return df
 
-# Adjust CPU speeds to be more consistent with the same units
-def normaliseCPU(cpu_speed):
-    cpu_speed = str(cpu_speed)
-    match = re.search(r'((\d+\.\d+|\d+))', cpu_speed)
-    if match:
-        value = float(match.group())
-        if value > 6:
-            value /= 1000
-df['cpu_speed'] = df['cpu_speed'].apply(normaliseCPU)
+def outputDataToFile(df):
+    df.to_excel(OUT_FILEPATH, index=False)  # index=False to exclude the index column in the output file
 
+if __name__ == "__main__":
+    df = pd.read_excel('amazon_laptop_2023.xlsx')
+    df = initialCleaning(df)
+    df = standardiseColours(df)
+    df = standardiseBrands(df)
+    df = cleanScreenSizes(df)
+    df = convertRamDiskSizesToGB(df)
+    df = standardiseOS(df)
+    df = normaliseCPUSpeeds(df)
 
+    outputDataToFile(df)
 
-# Testing using watch
-print("Total rows: ", df.shape[0])
+    print("Total rows: ", df.shape[0])
 
-unique_elems = Counter(df['model'].dropna().tolist())
-for el in unique_elems:
-    print(el)
+    {
+    # unique_elems = Counter(df['model'].dropna().tolist())
+    # for el in unique_elems:
+    #     print(el)
 
-# Most common words in the model column
-# print(Counter(' '.join(df['model'].astype(str).apply(lambda x: ' '.join(word for word in x.split() if word.isalpha())).fillna('')).split()))
-
-
-
-def outputDataToFile():
-    output_file_name = 'output_data.xlsx'
-
-    df.to_excel(output_file_name, index=False)  # index=False to exclude the index column in the output file
-outputDataToFile()
+    # Most common words in the model column
+    # print(Counter(' '.join(df['model'].astype(str).apply(lambda x: ' '.join(word for word in x.split() if word.isalpha())).fillna('')).split()))  
+    }
